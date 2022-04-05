0797f1a895b5b25e5ba9d8ab0ed93b5c90682025
Blazej <blazej.marciniak@deepsense.ai>
2022-04-05 08:43:38 +0200

    Add 3 35

--- chapter_3_machine_level_representation_of_programs/pp_3_28/pp_3_28.c
+++ chapter_3_machine_level_representation_of_programs/pp_3_28/pp_3_28.c
@@ -11,3 +11,18 @@ long fun_b(unsigned long x) {
 // 2. The compiler saw that the initialization of i is with the value of 64.
 // Therefore, a test i != was not required.
 // 3. The function computes reversal of bits in x
+
+long func() {
+    long sum = 0;
+    long i=0;
+
+    while (i<10) {
+        if (i&1) goto increment;
+        sum +=i;
+        increment:
+        i++;
+    }
+
+
+    return sum;
+}
